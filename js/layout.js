72c49f64798b328dd6c6b2c181c46ef59483bc01
htgame878 <hossien.T878@gmail.com>
2025-12-03 21:48:16 -1200

    2.0

--- js/layout.js
+++ js/layout.js
@@ -1,32 +1,39 @@
 // js/layout.js
 
 function createSidebar() {
-    const sidebar = document.createElement('div');
-    sidebar.className = 'sidebar';
-    
-    // تشخیص اینکه الان در کدام صفحه هستیم برای اکتیو کردن منو
-    const path = window.location.pathname;
-    
-    sidebar.innerHTML = `
+  const sidebar = document.createElement("div");
+  sidebar.className = "sidebar";
+
+  // تشخیص اینکه الان در کدام صفحه هستیم برای اکتیو کردن منو
+  const path = window.location.pathname;
+
+  sidebar.innerHTML = `
         <h2 style="margin-bottom: 30px; color: var(--primary);">سیستم هوشمند</h2>
         <nav>
-            <a href="dashboard.html" class="nav-item ${path.includes('dashboard.html') ? 'active' : ''}">
-                <span>🏠</span> داشبورد
+            <a href="dashboard.html" class="nav-item ${
+              path.includes("dashboard.html") ? "active" : ""
+            }">
+                <span>🏠</span> پروفایل شخصی
             </a>
-            <a href="classes.html" class="nav-item ${path.includes('classes.html') ? 'active' : ''}">
+            <a href="classes.html" class="nav-item ${
+              path.includes("classes.html") ? "active" : ""
+            }">
                 <span>👨‍🎓</span> مدیریت کلاس‌ها
+            </a>
+              <a href="classes.html" class="nav-item ">
+                <span>👨‍🎓</span> مدیریت آزمون ها
             </a>
             <a href="#" class="nav-item">
                 <span>📊</span> تحلیل‌ها
             </a>
-            <a href="#" class="nav-item">
+            <a href="settings.html" class="nav-item ${path.includes("settings.html") ? "active" : "" }">
                 <span>⚙️</span> تنظیمات
             </a>
         </nav>
     `;
-    
-    // اضافه کردن به اول بادی
-    document.body.prepend(sidebar);
+
+  // اضافه کردن به اول بادی
+  document.body.prepend(sidebar);
 }
 
 // اجرا شدن تابع به محض لود شدن فایل
